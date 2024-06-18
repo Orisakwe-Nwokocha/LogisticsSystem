86b565e8b22c321f3852cf2962c75e347659256b
josephEmeka <josephemekaonah@gmail.com>
2024-06-18 23:56:37 +0100

    Deliver_Information_Model

--- logistics/serializers.py
+++ logistics/serializers.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from rest_framework import serializers
 from .models import Package
 
@@ -7,3 +8,18 @@ class PackageSerializer(serializers.ModelSerializer):
         model = Package
         fields = ['tracking_number', 'weight',
                   'delivery_date', 'status']
+=======
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+>>>>>>> 41fbeba (Deliver_Information_Model)
