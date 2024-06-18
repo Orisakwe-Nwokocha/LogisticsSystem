0f2d65422d5b9853b32bf285b09e1ce32f123644
Orisakwe Nwokocha <orisakwenwokocha1@gmail.com>
2024-06-18 13:06:33 +0100

    added the tracker entity

--- logistics/models.py
+++ logistics/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Package(models.Model):
+    pass
+
+
+class Driver:
+    pass
+
+
+class ProgressReport(models.Model):
+    package = models.ForeignKey(Package, on_delete=models.CASCADE)
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    location = models.TextField()
+    description = models.TextField()
+    report_date = models.DateTimeField(auto_now_add=True)
+
+
+class Tracker(models.Model):
+    package = models.ForeignKey(Package, on_delete=models.CASCADE)
+    progress_reports = models.ManyToManyField(ProgressReport)
