911126c45cb0d74bf52861cf49258357dfe00c2d
Orisakwe Nwokocha <orisakwenwokocha1@gmail.com>
2024-06-19 15:46:04 +0100

    refactored models module

--- logistics/models.py
+++ logistics/models.py
@@ -1,25 +1,23 @@
 from django.db import models
 
-
 from logistics.utility import generate_tracking_number
 
 
 # Create your models here.
 
 class DeliveryInformation(models.Model):
-    sender_name = models.CharField(max_length = 225)
-    sender_address = models.CharField(max_length = 225)
-    receiver_name = models.CharField(max_length = 225)
-    receiver_address = models.CharField(max_length = 225)
-    delivery_fee = models.DecimalField(max_digits =100, decimal_places =2)
+    sender_name = models.CharField(max_length=225)
+    sender_address = models.TextField()
+    receiver_name = models.CharField(max_length=225)
+    receiver_address = models.TextField()
+    delivery_fee = models.DecimalField(max_digits=10, decimal_places=2)
     delivery_notes = models.TextField()
-    DELIVERY_TYPE_CHOICES = (
-        ('same day delivery', 'Express'),
-        ('next_day_delivery', 'Standard'),
-    )
-    delivery_type = models.CharField(max_length = 20, choices =DELIVERY_TYPE_CHOICES)
-    pickup_address = models.CharField(max_length = 225)
-    package = models.TextField()
+    DELIVERY_TYPE_CHOICES = [
+        ('E', 'EXPRESS'),
+        ('S', 'STANDARD'),
+    ]
+    delivery_type = models.CharField(max_length=1, choices=DELIVERY_TYPE_CHOICES, default='E')
+    pickup_address = models.TextField()
 
 
 class Package(models.Model):
@@ -28,15 +26,12 @@ class Package(models.Model):
         ('S', 'SUCCESSFUL'),
         ('F', 'FAILED'),
     ]
-    tracking_number = models.CharField(max_length=15,
-                                       unique=True,
+    tracking_number = models.CharField(max_length=15, unique=True,
                                        default=generate_tracking_number)
     deliveryInformation = models.ForeignKey(DeliveryInformation, on_delete=models.CASCADE)
     weight = models.DecimalField(max_digits=10, decimal_places=2)
     delivery_date = models.DateTimeField(null=True, blank=True)
-    status = models.CharField(max_length=15,
-                              choices=STATUS,
-                              default='P')
+    status = models.CharField(max_length=15, choices=STATUS, default='P')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -55,8 +50,3 @@ class ProgressReport(models.Model):
     description = models.TextField()
     report_date = models.DateTimeField(auto_now_add=True)
     tracker = models.ForeignKey(Tracker, on_delete=models.CASCADE, related_name="progress_reports")
-
-
-
-
-
