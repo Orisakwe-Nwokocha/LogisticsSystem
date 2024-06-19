e8e1ccc81449844d85497eb9c563db8277575b61
josephEmeka <josephemekaonah@gmail.com>
2024-06-19 15:17:10 +0100

    Delivery-model

--- logistics/models.py
+++ logistics/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 
+
 from logistics.utility import generate_tracking_number
 
 
 # Create your models here.
 
 class DeliveryInformation(models.Model):
-    pass
+    sender_name = models.CharField(max_length = 225)
+    sender_address = models.CharField(max_length = 225)
+    receiver_name = models.CharField(max_length = 225)
+    receiver_address = models.CharField(max_length = 225)
+    delivery_fee = models.DecimalField(max_digits =100, decimal_places =2)
+    delivery_notes = models.TextField()
+    DELIVERY_TYPE_CHOICES = (
+        ('same day delivery', 'Express'),
+        ('next_day_delivery', 'Standard'),
+    )
+    delivery_type = models.CharField(max_length = 20, choices =DELIVERY_TYPE_CHOICES)
+    pickup_address = models.CharField(max_length = 225)
+    package = models.TextField()
 
 
 class Package(models.Model):
@@ -42,3 +55,10 @@ class ProgressReport(models.Model):
     description = models.TextField()
     report_date = models.DateTimeField(auto_now_add=True)
     tracker = models.ForeignKey(Tracker, on_delete=models.CASCADE, related_name="progress_reports")
+
+
+# Create your models here.
+
+
+
+
