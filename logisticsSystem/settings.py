a55790a0ff95fa905e74a6875370f1b2f8736a1b
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-06-19 11:21:34 +0100

    Merged

--- logisticsSystem/settings.py
+++ logisticsSystem/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from django.core.checks import templates
+from django.template.utils.EngineHandler import templates
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -56,8 +59,7 @@ ROOT_URLCONF = 'logisticsSystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
