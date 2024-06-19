9e18814e635cca444d0da6a16cc5eccc31c60e12
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-06-19 09:17:52 +0100

    Package model fields

--- logistics/models.py
+++ logistics/models.py
@@ -5,7 +5,10 @@ from django.db import models
 
 
 class Package(models.Model):
-    pass
+    tracking_number = models.CharField(max_length=50, unique=True)
+    sender_name = models.CharField(max_length=20)
+    sender_address = models.TextField()
+
 
 
 class Driver:
