9fb9724e3841bd75e241bcbb832dce128e6bfcf0
Abolaji2k21 <abisoyeabolaji2k21@gmail.com>
2024-06-22 11:01:27 +0100

    admin.register(driver)

--- logistics/admin.py
+++ logistics/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from logistics.models import Package, DeliveryInformation
+from logistics.models import Package, DeliveryInformation, Driver
 
 
 # Register your models here.
@@ -19,3 +19,14 @@ class DeliveryInformationAdmin(admin.ModelAdmin):
     list_per_page = 10
     search_fields = ['sender_name', 'receiver_name', 'receiver_address', 'delivery_type']
     list_editable = ['receiver_name', 'receiver_address', 'delivery_type']
+
+
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ['driver_name', 'driver_address', 'username', 'phone_number', 'availability', 'date_registered']
+    list_per_page = 10
+    search_fields = ['driver_name', 'driver_address', 'username', 'phone_number', 'availability', 'date_registered']
+    list_editable = ['driver_name', 'driver_address', 'availability', ]
+
+
+
